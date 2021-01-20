8bc0027a1184f4c75a5135e26fcc0ee235eb268b
MDC <markdcross@gmail.com>
2021-01-20 16:15:21 -0500

    Unhandled promise rejection handler added

--- config/db.js
+++ config/db.js
@@ -2,14 +2,17 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/perspektiv", {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
-    });
+    const conn = await mongoose.connect(
+      process.env.MONGODB_URI || 'mongodb://localhost/perspektiv',
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+      }
+    );
 
-    console.log('MongoDB Connected...');
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(err.message);
     // Exit process with failure
--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 
-const app = express();
-
 // Load env vars
 dotenv.config();
 
 // Connect Database
 connectDB();
+
+const app = express();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
@@ -34,3 +34,10 @@ app.listen(PORT, () =>
     `Server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`
   )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
