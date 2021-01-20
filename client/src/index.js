692ea1fb32110ea50e627facb1098634b3d11924
Josh Allan <allan.josh07@gmail.com>
2021-01-19 20:51:29 -0500

    Added the @auth0/auth0-react npm package and wrapped the main App component in our AuthProvider. Will fully flesh out authentication soon.

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById('root')
 );
 
