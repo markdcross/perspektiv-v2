2a80c93e4f1f6cafe0d8d9e6486c1dc1da1de81d
MDC <markdcross@gmail.com>
2021-01-19 21:27:58 -0500

    Server up and connected to DB

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/perspektiv", {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
