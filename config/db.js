1cd0bbec9ba4a4237ce919638b1455702c311424
MDC <markdcross@gmail.com>
2021-01-20 16:26:32 -0500

    Added colors package

--- config/db.js
+++ config/db.js
@@ -1,23 +1,17 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(
-      process.env.MONGODB_URI || 'mongodb://localhost/perspektiv',
-      {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-      }
-    );
+  const conn = await mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/perspektiv',
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    }
+  );
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error(err.message);
-    // Exit process with failure
-    process.exit(1);
-  }
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
 };
 
 module.exports = connectDB;
