9526f28b6d6ca105eb022b8077a4866cfa62dc25
MDC <markdcross@gmail.com>
2021-01-21 13:10:48 -0500

    Added async/await handler

--- controllers/murals.js
+++ controllers/murals.js
@@ -1,101 +1,82 @@
 const ErrorResponse = require('../utils/ErrorResponse');
 const Mural = require('../models/Mural');
+const asyncHandler = require('../middleware/async');
 
 //* ======================================
 //*   @route    GET /api/v1/murals
 //!   @desc     Get All Murals
 //*   @access   Public
 //* ======================================
-exports.getMurals = async (req, res, next) => {
-  try {
-    const murals = await Mural.find();
+exports.getMurals = asyncHandler(async (req, res, next) => {
+  const murals = await Mural.find();
 
-    res.status(200).json({ success: true, count: murals.length, data: murals });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(200).json({ success: true, count: murals.length, data: murals });
+});
 
 //* ======================================
 //*   @route    GET /api/v1/murals/:id
 //!   @desc     Get single Mural
 //*   @access   Public
 //* ======================================
-exports.getMural = async (req, res, next) => {
-  try {
-    const mural = await Mural.findById(req.params.id);
+exports.getMural = asyncHandler(async (req, res, next) => {
+  const mural = await Mural.findById(req.params.id);
 
-    if (!mural) {
-      return next(
-        new ErrorResponse(`Mural not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({ success: true, data: mural });
-  } catch (err) {
-    next(err);
+  if (!mural) {
+    return next(
+      new ErrorResponse(`Mural not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: mural });
+});
 
 //* ======================================
 //*   @route    POST /api/v1/murals
 //!   @desc     Create new Mural
 //*   @access   Private
 //* ======================================
-exports.createMural = async (req, res, next) => {
-  try {
-    const mural = await Mural.create(req.body);
+exports.createMural = asyncHandler(async (req, res, next) => {
+  const mural = await Mural.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: mural
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: mural
+  });
+});
 
 //* ======================================
 //*   @route    PUT /api/v1/murals/:id
 //!   @desc     Update Mural
 //*   @access   Private
 //* ======================================
-exports.updateMural = async (req, res, next) => {
-  try {
-    const mural = await Mural.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+exports.updateMural = asyncHandler(async (req, res, next) => {
+  const mural = await Mural.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    if (!mural) {
-      return next(
-        new ErrorResponse(`Mural not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({ success: true, data: mural });
-  } catch (error) {
-    next(err);
+  if (!mural) {
+    return next(
+      new ErrorResponse(`Mural not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: mural });
+});
 
 //* ======================================
 //*   @route    DELETE /api/v1/murals/:id
 //!   @desc     Delete Mural
 //*   @access   Private
 //* ======================================
-exports.deleteMural = async (req, res, next) => {
-  try {
-    const mural = await Mural.findByIdAndDelete(req.params.id);
+exports.deleteMural = asyncHandler(async (req, res, next) => {
+  const mural = await Mural.findByIdAndDelete(req.params.id);
 
-    if (!mural) {
-      return next(
-        new ErrorResponse(`Mural not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({ success: true, data: {} });
-  } catch (error) {
-    next(err);
+  if (!mural) {
+    return next(
+      new ErrorResponse(`Mural not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: {} });
+});
--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,106 +1,84 @@
 const ErrorResponse = require('../utils/ErrorResponse');
 const Restaurant = require('../models/Restaurant');
+const asyncHandler = require('../middleware/async');
 
 //* ======================================
 //*   @route    GET /api/v1/restaurants
 //!   @desc     Get All Restaurants
 //*   @access   Public
 //* ======================================
-exports.getRestaurants = async (req, res, next) => {
-  try {
-    const restaurants = await Restaurant.find();
+exports.getRestaurants = asyncHandler(async (req, res, next) => {
+  const restaurants = await Restaurant.find();
 
-    res
-      .status(200)
-      .json({ success: true, count: restaurants.length, data: restaurants });
-  } catch (err) {
-    next(err);
-  }
-};
+  res
+    .status(200)
+    .json({ success: true, count: restaurants.length, data: restaurants });
+});
 
 //* ======================================
 //*   @route    GET /api/v1/restaurants/:id
 //!   @desc     Get single Restaurant
 //*   @access   Public
 //* ======================================
-exports.getRestaurant = async (req, res, next) => {
-  try {
-    const restaurant = await Restaurant.findById(req.params.id);
+exports.getRestaurant = asyncHandler(async (req, res, next) => {
+  const restaurant = await Restaurant.findById(req.params.id);
 
-    if (!restaurant) {
-      return next(
-        new ErrorResponse(
-          `Restaurant not found with id of ${req.params.id}`,
-          404
-        )
-      );
-    }
-
-    res.status(200).json({ success: true, data: restaurant });
-  } catch (err) {
-    next(err);
+  if (!restaurant) {
+    return next(
+      new ErrorResponse(`Restaurant not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: restaurant });
+});
 
 //* ======================================
 //*   @route    POST /api/v1/restaurants
 //!   @desc     Create new Restaurant
 //*   @access   Private
 //* ======================================
-exports.createRestaurant = async (req, res, next) => {
-  try {
-    const restaurant = await Restaurant.create(req.body);
+exports.createRestaurant = asyncHandler(async (req, res, next) => {
+  const restaurant = await Restaurant.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: restaurant
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: restaurant
+  });
+});
 
 //* ======================================
 //*   @route    PUT /api/v1/restaurants/:id
 //!   @desc     Update Restaurant
 //*   @access   Private
 //* ======================================
-exports.updateRestaurant = async (req, res, next) => {
-  try {
-    const restaurant = await Restaurant.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-        runValidators: true
-      }
-    );
-
-    if (!restaurant) {
-      return res.status(400).json({ success: false });
+exports.updateRestaurant = asyncHandler(async (req, res, next) => {
+  const restaurant = await Restaurant.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+      runValidators: true
     }
+  );
 
-    res.status(200).json({ success: true, data: restaurant });
-  } catch (error) {
-    next(err);
+  if (!restaurant) {
+    return res.status(400).json({ success: false });
   }
-};
+
+  res.status(200).json({ success: true, data: restaurant });
+});
 
 //* ======================================
 //*   @route    DELETE /api/v1/restaurants/:id
 //!   @desc     Delete Restaurant
 //*   @access   Private
 //* ======================================
-exports.deleteRestaurant = async (req, res, next) => {
-  try {
-    const restaurant = await Restaurant.findByIdAndDelete(req.params.id);
+exports.deleteRestaurant = asyncHandler(async (req, res, next) => {
+  const restaurant = await Restaurant.findByIdAndDelete(req.params.id);
 
-    if (!restaurant) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({ success: true, data: {} });
-  } catch (error) {
-    next(err);
+  if (!restaurant) {
+    return res.status(400).json({ success: false });
   }
-};
+
+  res.status(200).json({ success: true, data: {} });
+});
