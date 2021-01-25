3ae21bde62750030b6b1978fec7a9dc3ebe35c94
MDC <markdcross@gmail.com>
2021-01-25 16:28:26 -0500

    Added route authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -39,3 +39,18 @@ exports.protectedRoute = asyncHandler(async (req, res, next) => {
     );
   }
 });
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `User role ${req.user.role} is not authorized to access this route`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
