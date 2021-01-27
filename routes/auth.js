4bdbd6715fd133271dbc9a72627eec9d004bacb5
MDC <markdcross@gmail.com>
2021-01-27 09:14:17 -0500

    Update user name, email, password controller and routes

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,8 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
-
+  updateDetails,
+  updatePassword,
   logout
 } = require('../controllers/auth');
 
@@ -15,6 +16,8 @@ const { protectedRoute } = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protectedRoute, getMe);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 router.get('/logout', protectedRoute, logout);
