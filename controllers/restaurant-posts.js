8177c04e8edd70b7fdeef6be25b79f3d84d4510c
Josh Allan <allan.josh07@gmail.com>
2021-01-27 12:31:42 -0500

    Created the new controllers/restaurants-posts.js file for managing all restaurant post logic on the backend.

--- controllers/restaurant-posts.js
+++ controllers/restaurant-posts.js
@@ -1,18 +1,19 @@
 const path = require('path');
 
 const ErrorResponse = require('../utils/errorResponse');
-const MuralPost = require('../models/MuralPost');
+const RestaurantPost = require('../models/RestaurantPost');
 const asyncHandler = require('../middleware/async');
 
 //* ======================================
-//*   @route    GET /api/v1/posts
-//*   @route    GET /api/v1/murals/:muralId/posts
-//!   @desc     Get Posts
+//*   @route    GET /api/v1/restaurants/:restaurantId/posts
+//!   @desc     Get Posts for a specific restaurant
 //*   @access   Private
 //* ======================================
 exports.getPosts = asyncHandler(async (req, res, next) => {
-  if (req.params.muralId) {
-    const posts = await MuralPost.find({ mural: req.params.muralId });
+  if (req.params.restaurantId) {
+    const posts = await RestaurantPost.find({
+      restaurant: req.params.restaurantId
+    });
 
     res.status(200).json({ success: true, count: posts.length, data: posts });
   } else {
@@ -21,65 +22,62 @@ exports.getPosts = asyncHandler(async (req, res, next) => {
 });
 
 //* ======================================
-//*   @route    GET /api/v1/mural-posts/:id
+//*   @route    GET /api/v1/restaurant-posts/:id
 //!   @desc     Get single post
 //*   @access   Private
 //* ======================================
 exports.getPost = asyncHandler(async (req, res, next) => {
-  // grab the specific post by passing in the request parameter
-  const post = await MuralPost.findById(req.params.id).populate({
-    path: 'mural',
+  // grab the specific post by passing in the req parameter
+  const post = await RestaurantPost.findById(req.params.id).populate({
+    path: 'restaurant',
     select: 'name address'
   });
   // if there is no post with that ID, then return a 404 error
   if (!post) {
     return next(
-      new ErrorResponse(`Post not found with id of ${req.params.id}`, 404)
+      new ErrorResponse(`Post not found with the id of ${req.params.id}`, 404)
     );
   }
   res.status(200).json({ success: true, data: post });
 });
 
 //* ======================================
-//*   @route    POST /api/v1/murals/:muralId/posts
+//*   @route    POST /api/v1/restaurants/:restaurantId/posts
 //!   @desc     Create a new Post
 //*   @access   Private
 //* ======================================
 exports.createPost = asyncHandler(async (req, res, next) => {
-  req.body.mural = req.params.muralId;
+  req.body.restaurant = req.params.restaurandId;
   req.body.user = req.user.id;
 
-  const mural = await Mural.findById(req.params.muralId);
+  const restaurant = await Restaurant.findById(req.params.restaurandId);
 
-  if (!mural) {
+  if (!restaurant) {
     return next(
-      new ErrorResponse(`No mural with the id of ${req.params.muralId}`, 404)
+      new ErrorResponse(`No restaurant with the id of ${req.params.id}`, 404)
     );
   }
 
-  const post = await MuralPost.create(req.body);
+  const post = await RestaurantPost.create(req.body);
 
-  res.status(200).json({
-    success: true,
-    data: post
-  });
+  res.status(200).json({ success: true, data: post });
 });
 
 //* ======================================
-//*   @route    PUT /api/v1/mural-posts/:id
+//*   @route    PUT /api/v1/restaurant-posts/:id
 //!   @desc     Update Post
 //*   @access   Private
 //* ======================================
 exports.updatePost = asyncHandler(async (req, res, next) => {
-  let post = await MuralPost.findById(req.params.id);
+  let post = await RestaurantPost.findById(req.params.id);
 
   if (!post) {
     return next(
-      new ErrorResponse(`Post not found with id of ${req.params.id}`, 404)
+      new ErrorResponse(`Post not found with the id of ${req.params.id}`, 404)
     );
   }
 
-  // Make sure user is post owner
+  // make sure the user is the owner of the post
   if (post.user.toString() !== req.user.id && req.user.role !== 'admin') {
     return next(
       new ErrorResponse(
@@ -89,7 +87,7 @@ exports.updatePost = asyncHandler(async (req, res, next) => {
     );
   }
 
-  post = await MuralPost.findByIdAndUpdate(req.params.id, req.body, {
+  post = await RestaurantPost.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true
   });
@@ -98,12 +96,12 @@ exports.updatePost = asyncHandler(async (req, res, next) => {
 });
 
 //* ======================================
-//*   @route    DELETE /api/v1/mural-posts/:id
+//*   @route    DELETE /api/v1/restaurant-posts/:id
 //!   @desc     Delete Post
 //*   @access   Private
 //* ======================================
 exports.deletePost = asyncHandler(async (req, res, next) => {
-  const post = await MuralPost.findById(req.params.id);
+  const post = await RestaurantPost.findById(req.params.id);
 
   if (!post) {
     return next(
@@ -111,7 +109,7 @@ exports.deletePost = asyncHandler(async (req, res, next) => {
     );
   }
 
-  // Make sure user is post owner
+  // make sure user is post owner
   if (post.user.toString() !== req.user.id && req.user.role !== 'admin') {
     return next(
       new ErrorResponse(
@@ -127,12 +125,12 @@ exports.deletePost = asyncHandler(async (req, res, next) => {
 });
 
 //* ======================================
-//*   @route    PUT /api/v1/mural-posts/:id/photo
+//*   @route    PUT /api/v1/restaurant-posts/:id/photo
 //!   @desc     Upload photo for post
 //*   @access   Private
 //* ======================================
 exports.postPhotoUpload = asyncHandler(async (req, res, next) => {
-  const post = await MuralPost.findById(req.params.id);
+  const post = await RestaurantPost.findById(req.params.id);
 
   if (!post) {
     return next(
@@ -140,7 +138,7 @@ exports.postPhotoUpload = asyncHandler(async (req, res, next) => {
     );
   }
 
-  // Make sure user is post owner OR an admin
+  // make sure the user is post owner or an admin
   if (post.user.toString() !== req.user.id && req.user.role !== 'admin') {
     return next(
       new ErrorResponse(
@@ -180,7 +178,7 @@ exports.postPhotoUpload = asyncHandler(async (req, res, next) => {
       return next(new ErrorResponse(`Problem with file upload`, 500));
     }
 
-    await MuralPost.findByIdAndUpdate(req.params.id, { image: file.name });
+    await RestaurantPost.findByIdAndUpdate(req.params.id, { image: file.name });
 
     res.status(200).json({
       success: true,
