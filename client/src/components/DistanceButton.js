2889d6a61c5f02ebc1d73add13fe78b81109827c
MDC <markdcross@gmail.com>
2021-01-30 10:43:27 -0500

    Added units to mural distance

--- client/src/components/DistanceButton.js
+++ client/src/components/DistanceButton.js
@@ -31,7 +31,7 @@ const DistanceButton = ({ location }) => {
           basic: true,
           color: 'yellow',
           pointing: 'left',
-          content: distance
+          content: `${distance}m`
         }}
       />
     </Fragment>
