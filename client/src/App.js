3d546aec92d482bc506786392085b09485132ff0
Bertodemus <roberto@robertorubet.com>
2021-02-01 14:31:44 -0500

    corrected issue with import in APP.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,51 +3,51 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import LocationContext from "./context/LocationContext";
 import MuralContext from "./context/MuralContext";
+import { AuthProvider } from "./context/AuthContext";
 import muralsAPI from "./utils/murals-API";
 import history from "./components/History";
-// import { AuthProvider } from "./context/AuthContext";
 // import authAPI from './utils/auth-API';
 import { usePosition } from "use-position";
 import { Container } from "react-bootstrap";
 
 const App = () => {
-  const [muralState, setMuralState] = useState([]);
-  // const [userState, setUserState] = useState();
+	const [muralState, setMuralState] = useState([]);
+	// const [userState, setUserState] = useState();
 
-  useEffect(() => {
-    // // check for a currently logged in user from the api
-    // authAPI.getCurrentUser().then((data) => {
-    //   // if the check comes back as a success, then set the userState object to the logged in user and set isAuthenticated to true
-    //   if (data.status === 200) {
-    //     setUserState({ data, isAuthenticated: true });
-    //   }
-    //   // otherwise, set isAuthenticated to false
-    //   else {
-    //     setUserState({ isAuthenticated: false });
-    //   }
-    // });
+	useEffect(() => {
+		// // check for a currently logged in user from the api
+		// authAPI.getCurrentUser().then((data) => {
+		//   // if the check comes back as a success, then set the userState object to the logged in user and set isAuthenticated to true
+		//   if (data.status === 200) {
+		//     setUserState({ data, isAuthenticated: true });
+		//   }
+		//   // otherwise, set isAuthenticated to false
+		//   else {
+		//     setUserState({ isAuthenticated: false });
+		//   }
+		// });
 
-    // get the murals from the api
-    muralsAPI.getMurals().then((data) => {
-      setMuralState(data);
-    });
-  }, []);
+		// get the murals from the api
+		muralsAPI.getMurals().then((data) => {
+			setMuralState(data);
+		});
+	}, []);
 
-  const position = usePosition();
+	const position = usePosition();
 
-  return (
-    <AuthProvider>
-      <MuralContext.Provider value={muralState}>
-        <LocationContext.Provider value={position}>
-				<Router history={history}>
-					<Container fluid>
-						<Route path='/' component={Home} />
-					</Container>
-				</Router>
-        </LocationContext.Provider>
-      </MuralContext.Provider>
-    </AuthProvider>
-  );
+	return (
+		<AuthProvider>
+			<MuralContext.Provider value={muralState}>
+				<LocationContext.Provider value={position}>
+					<Router history={history}>
+						<Container fluid>
+							<Route path="/" component={Home} />
+						</Container>
+					</Router>
+				</LocationContext.Provider>
+			</MuralContext.Provider>
+		</AuthProvider>
+	);
 };
 
 export default App;
