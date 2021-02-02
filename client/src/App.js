fe6cded870b1b717047bd049d2d34de62abdd470
Bertodemus <roberto@robertorubet.com>
2021-02-02 02:50:14 -0500

    update to format

--- client/src/App.js
+++ client/src/App.js
@@ -1,87 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 // import NavTabs from "./components/NavTabs";
-import LocationContext from './context/LocationContext';
-import MuralContext from './context/MuralContext';
+import LocationContext from "./context/LocationContext";
+import MuralContext from "./context/MuralContext";
 
 // NEW auth context provider
-import AuthState from './context/auth-v2/AuthState';
-import AlertState from './context/alert/AlertState';
+import AuthState from "./context/auth-v2/AuthState";
+import AlertState from "./context/alert/AlertState";
 
 // page components
-import Home from './pages/Home';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
+import Home from "./pages/Home";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 // OLD auth context provider
 // import { AuthProvider } from './context/AuthContext';
 
 // mural API
-import muralsAPI from './utils/murals-API';
+import muralsAPI from "./utils/murals-API";
 
 // import authAPI from './utils/auth-API';
 
 // utils
-import history from './components/History';
-import { usePosition } from 'use-position';
-import { Container } from 'react-bootstrap';
+import history from "./components/History";
+import { usePosition } from "use-position";
+import { Container } from "react-bootstrap";
 
 // Loading Screen
-import LoadingScreen from './components/Loading'
+import LoadingScreen from "./components/Loading";
 
 const App = () => {
-  const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 2000)
-  }, [])
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		setTimeout(() => setLoading(false), 2000);
+	}, []);
 
-  const [muralState, setMuralState] = useState([]);
-  // const [userState, setUserState] = useState();
+	const [muralState, setMuralState] = useState([]);
+	// const [userState, setUserState] = useState();
 
-  useEffect(() => {
-    // get the murals from the api
-    muralsAPI.getMurals().then((data) => {
-      setMuralState(data);
-    });
-  }, []);
+	useEffect(() => {
+		// get the murals from the api
+		muralsAPI.getMurals().then((data) => {
+			setMuralState(data);
+		});
+	}, []);
 
-  const position = usePosition();
+	const position = usePosition();
 
-  return (
-    <>
-    {loading === false ? (
-    <MuralContext.Provider value={muralState}>
-    <LocationContext.Provider value={position}>
-      <Router history={history}>
-        <Container fluid>
-          <Route path='/' component={Home} />
-        </Container>
-      </Router>
-    </LocationContext.Provider>
-  </MuralContext.Provider>
-        ) : (
-          <LoadingScreen />
-        )}
-        </>
-    // <AuthState>
-    //   <AlertState>
-    //     {/* <AuthProvider> */}
-    //     <MuralContext.Provider value={muralState}>
-    //       <LocationContext.Provider value={position}>
-    //         <Router history={history}>
-    //           <Container fluid>
-    //             <Route exact path="/" component={Home} />
-    //             <Route exact path="/register" component={Register} />
-    //             <Route exact path="/login" component={Login} />
-    //           </Container>
-    //         </Router>
-    //       </LocationContext.Provider>
-    //     </MuralContext.Provider>
-    //     {/* </AuthProvider> */}
-    //   </AlertState>
-    // </AuthState>
-  );
+	return (
+		<>
+			{loading === false ? (
+				<MuralContext.Provider value={muralState}>
+					<LocationContext.Provider value={position}>
+						<Router history={history}>
+							<Container fluid>
+								<Route path='/' component={Home} />
+							</Container>
+						</Router>
+					</LocationContext.Provider>
+				</MuralContext.Provider>
+			) : (
+				<LoadingScreen />
+			)}
+		</>
+		// <AuthState>
+		//   <AlertState>
+		//     {/* <AuthProvider> */}
+		//     <MuralContext.Provider value={muralState}>
+		//       <LocationContext.Provider value={position}>
+		//         <Router history={history}>
+		//           <Container fluid>
+		//             <Route exact path="/" component={Home} />
+		//             <Route exact path="/register" component={Register} />
+		//             <Route exact path="/login" component={Login} />
+		//           </Container>
+		//         </Router>
+		//       </LocationContext.Provider>
+		//     </MuralContext.Provider>
+		//     {/* </AuthProvider> */}
+		//   </AlertState>
+		// </AuthState>
+	);
 };
 
 export default App;
