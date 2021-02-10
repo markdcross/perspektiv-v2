1d2b997feeb1c351fbc64a609d0d6799dd28462d
Bertodemus <roberto@robertorubet.com>
2021-02-09 19:00:57 -0500

    updates to animations

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -5,19 +5,19 @@ export default function Loading() {
 	return (
 		<Container className='h-100 d-flex justify-content-center'>
 			<Row className='text-center mt-5'>
-				<Col xs={12}>
+				{/* <Col xs={12}>
 					<Image
 						src={`../../assets/images/logo/plogo.png`}
 						alt={"logo png"}
 						width={350}
 					/>
-				</Col>
+				</Col> */}
 				<Col xs={12} className="mt-0">
 					<Image
-						src={`../../assets/images/gif/loading.gif`}
+						src={`../../assets/images/gif/PerspektivScript.gif`}
 						alt={"spinning gif"}
 						className='clearfix'
-						width={200}
+						width={350}
 					/>
 				</Col>
 			</Row>
