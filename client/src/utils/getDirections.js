0a0844662725004420889a83d7f4db21c5161135
MDC <markdcross@gmail.com>
2021-02-10 08:52:51 -0500

    Deployment cors issue troubleshooting

--- client/src/utils/getDirections.js
+++ client/src/utils/getDirections.js
@@ -6,7 +6,12 @@ export default {
   // get all murals
   getDirections: function (lat, lng, muralLat, muralLng) {
     return axios.get(
-      `https://www.mapquestapi.com/directions/v2/route?key=Har1ZZKjsFe24ZZSfUVi3uIQ0Dy1agEi&from=${lat},${lng}&to=${muralLat},${muralLng}+&routeType=pedestrian`
+      `https://www.mapquestapi.com/directions/v2/route?key=Har1ZZKjsFe24ZZSfUVi3uIQ0Dy1agEi&from=${lat},${lng}&to=${muralLat},${muralLng}+&routeType=pedestrian`,
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
     );
   }
 };
