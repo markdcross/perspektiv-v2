4cacd5809eeb689e3a393107a9a0ff767adeb1e3
MDC <markdcross@gmail.com>
2021-02-10 09:14:32 -0500

    Deployment cors issue troubleshooting

--- client/src/utils/getDirections.js
+++ client/src/utils/getDirections.js
@@ -9,7 +9,7 @@ export default {
       `https://www.mapquestapi.com/directions/v2/route?key=Har1ZZKjsFe24ZZSfUVi3uIQ0Dy1agEi&from=${lat},${lng}&to=${muralLat},${muralLng}+&routeType=pedestrian`,
       {
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/x-www-form-urlencoded'
         }
       }
     );
