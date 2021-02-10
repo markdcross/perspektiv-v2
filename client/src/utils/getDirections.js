26375ed160a4957762537bc122451fd252353f52
MDC <markdcross@gmail.com>
2021-02-10 09:42:17 -0500

    Deployment cors issue troubleshooting

--- client/src/utils/getDirections.js
+++ client/src/utils/getDirections.js
@@ -5,15 +5,23 @@ import axios from 'axios';
 export default {
   // get all murals
   getDirections: function (lat, lng, muralLat, muralLng) {
-    return axios.get(
-      `https://www.mapquestapi.com/directions/v2/route?key=Har1ZZKjsFe24ZZSfUVi3uIQ0Dy1agEi&from=${lat},${lng}&to=${muralLat},${muralLng}+&routeType=pedestrian`,
-      {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      }
-    );
+    return axios({
+      method: 'GET',
+      url: `https://www.mapquestapi.com/directions/v2/route?key=Har1ZZKjsFe24ZZSfUVi3uIQ0Dy1agEi&from=${lat},${lng}&to=${muralLat},${muralLng}+&routeType=pedestrian`,
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    });
   }
 };
+
+//     get(
+//       `https://www.mapquestapi.com/directions/v2/route?key=Har1ZZKjsFe24ZZSfUVi3uIQ0Dy1agEi&from=${lat},${lng}&to=${muralLat},${muralLng}+&routeType=pedestrian`,
+//       {
+//         headers: {
+//           'Content-Type': 'application/x-www-form-urlencoded'
+//         }
+//       }
+//     );
+//   }
+// };
 
 // http://www.mapquestapi.com/directions/v2/route?key=Har1ZZKjsFe24ZZSfUVi3uIQ0Dy1agEi&from=${lat}+${lng}&to=${muralLat}=${muralLng}&routeType=pedestrian
