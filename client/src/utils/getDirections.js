35a66d9fcbc34430e4c93859edcb003c92d118e3
MDC <markdcross@gmail.com>
2021-02-10 15:06:48 -0500

    Cors resolution

--- client/src/utils/getDirections.js
+++ client/src/utils/getDirections.js
@@ -5,10 +5,14 @@ import axios from 'axios';
 export default {
   // get all murals
   getDirections: function (lat, lng, muralLat, muralLng) {
+    delete axios.defaults.headers.common['x-auth-token'];
     return axios({
       method: 'GET',
       url: `https://www.mapquestapi.com/directions/v2/route?key=Har1ZZKjsFe24ZZSfUVi3uIQ0Dy1agEi&from=${lat},${lng}&to=${muralLat},${muralLng}+&routeType=pedestrian`,
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+        // 'x-auth-token': undefined
+      }
     });
   }
 };
