9acf4eca7c4fa887e49c5c27dab5661e6a3ddec7
MDC <markdcross@gmail.com>
2021-02-09 21:32:49 -0500

    Deployment

--- client/src/utils/getDirections.js
+++ client/src/utils/getDirections.js
@@ -6,7 +6,7 @@ export default {
   // get all murals
   getDirections: function (lat, lng, muralLat, muralLng) {
     return axios.get(
-      `http://www.mapquestapi.com/directions/v2/route?key=Har1ZZKjsFe24ZZSfUVi3uIQ0Dy1agEi&from=${lat},${lng}&to=${muralLat},${muralLng}+&routeType=pedestrian`
+      `https://www.mapquestapi.com/directions/v2/route?key=Har1ZZKjsFe24ZZSfUVi3uIQ0Dy1agEi&from=${lat},${lng}&to=${muralLat},${muralLng}+&routeType=pedestrian`
     );
   }
 };
